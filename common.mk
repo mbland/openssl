21ef57bcd29b56d02dbb12873c003b8fadb7167b
Mike Bland <mbland@acm.org>
2014-06-22 14:18:46 -0400

    Remove OPENSSL_DOING_MAKEDEPEND symbol
    This actually causes breakages thanks to some symbols in opensslconf.h
    not getting turned on.

--- common.mk
+++ common.mk
@@ -1,7 +1,7 @@
 # Common commands and rules shared between Makefiles
 MAKEDEP_CMD=\
-	@set -e; rm -f $@; \
-	$(CC) -MM -DOPENSSL_DOING_MAKEDEPEND $(CFLAGS) $< > $@.$$$$; \
+	@set -e; rm -f $@ $@.*; \
+	$(CC) -MM $(CFLAGS) $< > $@.$$$$; \
 	sed 's,\($*\)\.o[ :]*,\1.o $@ : ,g' < $@.$$$$ > $@; \
 	rm -f $@.$$$$
 
